c0588a4960c167804e5aaacf74fc075256750c2f
gdipietro74 <gdipietro@gdipietro.domus.ad.sogei.it>
2021-05-04 11:41:39 +0200

    feat: refactoring eucert to dgc

---
+++ src/main/java/it/interop/dgc/gateway/entity/SignerUploadInformationEntity.java
@@ -0,0 +1,76 @@
+/*-
+ *   Copyright (C) 2021 Presidenza del Consiglio dei Ministri.
+ *   Please refer to the AUTHORS file for more information. 
+ *   This program is free software: you can redistribute it and/or modify 
+ *   it under the terms of the GNU Affero General Public License as 
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *   This program is distributed in the hope that it will be useful, 
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ *   GNU Affero General Public License for more details.
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program. If not, see <https://www.gnu.org/licenses/>.   
+ */
+package it.interop.dgc.gateway.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import it.interop.dgc.gateway.enums.CertificateType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Document(collection = "signer_upload_information")
+public class SignerUploadInformationEntity implements Serializable {
+	private static final long serialVersionUID = 3303522990191884259L;
+
+	@Id
+    private String id;
+
+    @Field(name="id")
+    private Long index;
+    
+    @Field(name="kid")
+    private String kid;
+    
+    @Field(name = "country")
+    private String country;
+
+    @Field(name = "thumbprint")
+    private String thumbprint;
+
+    @Field(name = "raw_data")
+    private String rawData;
+
+    @Field(name = "signature")
+    private String signature;
+
+    @Field(name = "certificate_type")
+    private CertificateType certificateType;
+    
+    @CreatedDate
+    @Field(name="created_at")
+    private Date createdAt;
+
+	@Field("revoked")
+	private boolean revoked;
+
+	@Field("revoked_date")
+	private Date revokedDate;
+
+    @Field(name="batch_tag")
+    private String uploadBatchTag;
+    
+    @Field(name="batch_tag_revoke")
+    private String revokedBatchTag;
+	
+	
+}
