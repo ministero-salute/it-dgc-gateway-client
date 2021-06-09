f0d262a24fc565b9b52fb20f0c9f1a762299ef98
Alfonso D'amelio <31849245+alfo1995@users.noreply.github.com>
2021-06-09 16:04:28 +0200

    feat: add import

--- src/main/java/it/interop/dgc/gateway/DgcGatewayItalyClientApplication.java
+++ src/main/java/it/interop/dgc/gateway/DgcGatewayItalyClientApplication.java
@@ -16,6 +16,7 @@ package it.interop.dgc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
