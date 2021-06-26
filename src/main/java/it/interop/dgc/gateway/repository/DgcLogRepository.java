ba2dc6dbd50aef8ceff1c57dbf2d2142270ab7f0
Matteo Baccan <matteo.baccan@gmail.com>
2021-06-26 16:05:06 +0200

    Removed unused imports

--- src/main/java/it/interop/dgc/gateway/repository/DgcLogRepository.java
+++ src/main/java/it/interop/dgc/gateway/repository/DgcLogRepository.java
@@ -16,8 +16,6 @@ package it.interop.dgc.gateway.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import it.interop.dgc.gateway.entity.DgcLogEntity;
