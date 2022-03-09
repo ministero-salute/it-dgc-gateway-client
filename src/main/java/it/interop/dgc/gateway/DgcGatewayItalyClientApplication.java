828800d5b19af735edf1f84f7da3f293b1c10a7f
lorenzocerini <lorenzocerini@lorenzos-air>
2022-03-09 14:21:23 +0100

    Fix - scheduler

--- src/main/java/it/interop/dgc/gateway/DgcGatewayItalyClientApplication.java
+++ src/main/java/it/interop/dgc/gateway/DgcGatewayItalyClientApplication.java
@@ -18,7 +18,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 public class DgcGatewayItalyClientApplication {
 
